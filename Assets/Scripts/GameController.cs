dc24b285dabf6a363bd8a47e01eb5c37b300380f
Kanchi0914 <dentipass205>
2019-03-07 16:36:46 +0900

    test
    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    Text scenarioMessage;
+    List<Scenario> scenarios = new List<Scenario>();
+
+    Scenario currentScenario;
+    int index = 0;
+
+    class Scenario
+    {
+        public string ScenarioID;
+        public List<string> Texts;
+        public List<string> Options;
+        public string NextScenarioID;
+    }
+
+    void Start ()
+    {
+        var scenario01 = new Scenario()
+        {
+            ScenarioID = "scenario01",
+            Texts = new List<string>()
+            {
+                "テスト文章１",
+                "テスト文章２",
+                "テスト文章３",
+                "テスト文章４",
+                "テスト文章５"
+            }
+        };
+
+        SetScenario(scenario01);
+    }
+    
+    void Update()
+    {
+        if (currentScenario != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SetNextMessage();
+            }
+        }
+    }
+
+    void SetScenario(Scenario scenario)
+    {
+        currentScenario = scenario;
+        scenarioMessage.text = currentScenario.Texts[0];
+    }
+
+    void SetNextMessage()
+    {
+        if (currentScenario.Texts.Count > index + 1)
+        {
+            index++;
+            scenarioMessage.text = currentScenario.Texts[index];
+        }
+        else
+        {
+            ExitScenario();
+        }
+    }
+
+    void ExitScenario()
+    {
+        scenarioMessage.text = "";
+        index = 0;
+        if (string.IsNullOrEmpty(currentScenario.NextScenarioID))
+        {
+            currentScenario = null;
+        }
+        else
+        {
+            var nextScenario = scenarios.Find
+                (s => s.ScenarioID == currentScenario.NextScenarioID);
+        }
+    }
+
+}
